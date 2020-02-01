cc86a68cf156cb9bbca9c84a837dfb1366b7553e
Jesse Rushlow <jr@geeshoe.com>
2020-02-01 06:29:14 -0500

    WIP generate some randomness

--- src/Generator/TokenGenerator.php
+++ src/Generator/TokenGenerator.php
@@ -26,4 +26,41 @@ class TokenGenerator
         /** @TODO Need something better */
         return new \Exception('OOPS');
     }
+
+    protected function generate()
+    {
+        //@TODO gen a token
+    }
+
+    /**
+     * Original credit to Laravel's Str::random() method.
+     */
+    protected function randomAlphaNumStr(int $length): string
+    {
+        $string = '';
+
+        while (($len = strlen($string)) < $length) {
+            $size = $length - $len;
+
+            /** @TODO ?Keep separated due to \Throwable? vs handle \Throwable in loop.. */
+//            $bytes = random_bytes($size);
+            $bytes = $this->getRandomBytes($size);
+
+            $string .= substr(
+                str_replace(['/', '+', '='], '', base64_encode($bytes)), 0, $size);
+        }
+
+        return $string;
+    }
+
+    protected function getRandomBytes(int $size): string
+    {
+        //@todo edge case: $size = 0 -> \Error
+        /** @TODO Bad oops */
+        try {
+            return \random_bytes($size);
+        } catch (\Exception $exception) {
+            throw $this->oops();
+        }
+    }
 }
--- src/tests/UnitTests/Generator/TokenGeneratorTest.php
+++ src/tests/UnitTests/Generator/TokenGeneratorTest.php
@@ -6,9 +6,18 @@ namespace SymfonyCasts\Bundle\ResetPassword\tests\UnitTests\Generator;
 
 use SymfonyCasts\Bundle\ResetPassword\Generator\TokenGenerator;
 use PHPUnit\Framework\TestCase;
+use SymfonyCasts\Bundle\ResetPassword\tests\Fixtures\TokenGeneratorTestFixture;
 
 class TokenGeneratorTest extends TestCase
 {
+    /** @var TokenGeneratorTestFixture */
+    public $fixture;
+
+    protected function setUp()
+    {
+        $this->fixture = new TokenGeneratorTestFixture();
+    }
+
     /** @test */
     public function throwsExceptionIfTokenNotInitialized(): void
     {
@@ -16,5 +25,37 @@ class TokenGeneratorTest extends TestCase
 
         $generator = new TokenGenerator();
         $generator->getToken();
+    }
+
+    /** @test */
+    public function randomStrReturned(): void
+    {
+        $resultA = $this->fixture->getRandomAlphaNumStr(20);
+        $resultB = $this->fixture->getRandomAlphaNumStr(20);
+
+        self::assertNotSame($resultA, $resultB);
+    }
+
+    /** @test */
+    public function randomStrReturnsCorrectLength(): void
+    {
+        $result = $this->fixture->getRandomAlphaNumStr(100);
+
+        self::assertSame(100, strlen($result));
+    }
+
+    /** @test */
+    public function RandomBytesThrowsExceptionWithBadSize(): void
+    {
+        $this->expectException(\Error::class);
+        $this->fixture->getRandomBytesFromProtected(0);
+    }
+
+    /** @test */
+    public function getRandomBytesUsesLength(): void
+    {
+        $result = $this->fixture->getRandomBytesFromProtected(100);
+
+        $this->assertSame(200, strlen(bin2hex($result)));
     }
 }
