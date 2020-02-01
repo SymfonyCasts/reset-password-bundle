b3c39c68f7bd2da380b387e403dd0af895c66325
Jesse Rushlow <jr@geeshoe.com>
2020-02-01 08:39:56 -0500

    remove un-needed use statement

--- src/Generator/TokenGenerator.php
+++ src/Generator/TokenGenerator.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace SymfonyCasts\Bundle\ResetPassword\Generator;
 
-use SymfonyCasts\Bundle\ResetPassword\Model\PasswordResetToken;
-
 /**
  * Generate hashed HMAC token
  *
