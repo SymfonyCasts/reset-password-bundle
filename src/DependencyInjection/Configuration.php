42285a3d35c929b523ab794ab3c23e4e3c19537c
Jesse Rushlow <jr@geeshoe.com>
2020-02-03 16:41:32 -0500

    refactor password reset bundle config to use in app

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -20,6 +20,9 @@ final class Configuration implements ConfigurationInterface
                     ->isRequired()
                     ->info('A class that implements PasswordResetRequestRepositoryInterface - usually your PasswordResetTokenRepository.')
                 ->end()
+                ->scalarNode('signing_key')
+                    ->info('App key to sign tokens')
+                ->end()
                 ->integerNode('lifetime')
                     ->defaultValue(3600)
                     ->info('How long a reset password token should be valid before expiring.')
@@ -27,6 +30,9 @@ final class Configuration implements ConfigurationInterface
                 ->integerNode('request_throttle_time')
                     ->defaultValue(3600)
                     ->info('How long a reset password token should be valid before expiring.')
+                ->end()
+                ->scalarNode('token_generator')
+                    ->info('Generator used to create tokens')
                 ->end()
             ->end();
 
--- src/DependencyInjection/SymfonyCastsPasswordResetExtension.php
+++ src/DependencyInjection/SymfonyCastsPasswordResetExtension.php
@@ -15,11 +15,14 @@ class SymfonyCastsPasswordResetExtension extends Extension
         $loader = new XmlFileLoader($container, new FileLocator(\dirname(__DIR__).'/Resources/config'));
         $loader->load('password_reset_services.xml');
 
+
         $configuration = $this->getConfiguration($configs, $container);
+
         $config = $this->processConfiguration($configuration, $configs);
 
         $helperDefinition = $container->getDefinition('symfonycasts.reset_password.helper');
         $helperDefinition->replaceArgument(0, new Reference($config['password_request_repository']));
-        $helperDefinition->replaceArgument(3, $config['lifetime']);
+        $helperDefinition->replaceArgument(2, $config['lifetime']);
+        $helperDefinition->replaceArgument(3, $config['request_throttle_time']);
     }
 }
