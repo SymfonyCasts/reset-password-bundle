5d73a47a33fa4884c74561bb3356c7bc16146d8b
Jesse Rushlow <jr@geeshoe.com>
2020-02-04 06:01:26 -0500

    improve reset request interface method scalar type signatures

--- src/Persistence/PasswordResetRequestRepositoryInterface.php
+++ src/Persistence/PasswordResetRequestRepositoryInterface.php
@@ -2,15 +2,16 @@
 
 namespace SymfonyCasts\Bundle\ResetPassword\Persistence;
 
+use Symfony\Component\Security\Core\User\UserInterface;
 use SymfonyCasts\Bundle\ResetPassword\Model\PasswordResetRequestInterface;
 
 interface PasswordResetRequestRepositoryInterface
 {
-    public function createPasswordResetRequest(object $user, \DateTimeInterface $expiresAt, string $selector, string $hashedToken): PasswordResetRequestInterface;
+    public function createPasswordResetRequest(UserInterface $user, \DateTimeImmutable $expiresAt, string $selector, string $hashedToken): PasswordResetRequestInterface;
 
     public function persistPasswordResetRequest(PasswordResetRequestInterface $passwordResetRequest);
 
     public function findPasswordResetRequest(string $selector): ?PasswordResetRequestInterface;
 
-    public function getMostRecentNonExpiredRequestDate(object $user): ?\DateTimeInterface;
+    public function getMostRecentNonExpiredRequestDate(UserInterface $user): ?\DateTimeImmutable;
 }
