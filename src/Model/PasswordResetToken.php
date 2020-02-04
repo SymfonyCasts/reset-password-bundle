212901c86504232db8f8b25ce7bd0040cc9546d0
Jesse Rushlow <jr@geeshoe.com>
2020-02-04 17:42:22 -0500

    refactor PasswordResetToken::class
    marked class as final
    added symfony comments

--- src/Model/PasswordResetToken.php
+++ src/Model/PasswordResetToken.php
@@ -1,10 +1,23 @@
 <?php
 
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace SymfonyCasts\Bundle\ResetPassword\Model;
 
 use SymfonyCasts\Bundle\ResetPassword\Exception\EmptyTokenStringException;
 
-class PasswordResetToken
+/**
+ * @author Jesse Rushlow <jr@rushlow.dev>
+ * @author Ryan Weaver <weaverryan@gmail.com>
+ */
+final class PasswordResetToken
 {
     private $token;
     private $expiresAt;
