b4475d079620dac11b2529ba3732800e0196feb6
Jesse Rushlow <jr@geeshoe.com>
2020-02-05 10:22:53 -0500

    added copyright & authors

--- src/Model/PasswordResetRequestInterface.php
+++ src/Model/PasswordResetRequestInterface.php
@@ -1,7 +1,20 @@
 <?php
 
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace SymfonyCasts\Bundle\ResetPassword\Model;
 
+/**
+ * @author Jesse Rushlow <jr@rushlow.dev>
+ * @author Ryan Weaver <weaverryan@gmail.com>
+ */
 interface PasswordResetRequestInterface
 {
     public function getRequestedAt(): \DateTimeImmutable;
