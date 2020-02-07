14358287267ee41a3d1da80bc6699a759c3b2386
Jesse Rushlow <jr@geeshoe.com>
2020-02-07 12:08:12 -0500

    WIP - cleanup artifacts from dev, getting symfonyized

--- src/Generator/TokenGenerator.php
+++ src/Generator/TokenGenerator.php
@@ -1,57 +1,55 @@
 <?php
 
-declare(strict_types=1);
-
 namespace SymfonyCasts\Bundle\ResetPassword\Generator;
 
-use SymfonyCasts\Bundle\ResetPassword\Exception\TokenException;
-
 /**
- * Generate hashed HMAC token
- *
- * Doesn't care about anything else other than making a token
+ * @author Jesse Rushlow <jr@rushlow.dev>
+ * @author Ryan Weaver <weaverryan@gmail.com>
+ * @internal
  */
 class TokenGenerator
+//final class TokenGenerator @TODO make final? fix tests to do so
 {
+    //@TODO who was supposed to use me
     public const HMAC_HASH_ALGO = 'sha256';
 
-    /** @throws \Throwable */
-    public function getToken(
-        string $signingKey,
-        \DateTimeImmutable $expiresAt,
-        string $verifier,
-        string $userId
-    ): string {
-//        $this->isExpireValid($expiresAt);
-
+    public function getToken(string $signingKey, \DateTimeImmutable $expiresAt, string $verifier, string $userId): string
+    {
         return \hash_hmac(
             self::HMAC_HASH_ALGO,
             $this->encodeHashData($expiresAt, $verifier, $userId),
             $signingKey,
             false
         );
-//        return $this->generateHash($signingKey, $expiresAt, $verifier, $userId);
     }
 
-    /** @throws \Throwable */
-    private function isEmpty(string $value): void
+    //@todo make me private | fix tests for private
+    protected function encodeHashData(\DateTimeImmutable $expiresAt, string $verifier, string $userId): string
     {
-        //@TODO safe to remove after refactoring
-        if (empty($value)) {
-            throw TokenException::getIsEmpty();
-        }
+        return \json_encode([
+            $verifier,
+            $userId,
+            $expiresAt->format('Y-m-d\TH:i:s')
+        ]);
     }
 
-    /** @throws TokenException */
-    private function isExpireValid(\DateTimeImmutable $expire): void
-    {
-        //@TODO safe to remove after refactoring
-        $time = $expire->getTimestamp();
-
-        if ($time <= time()) {
-            throw TokenException::getInvalidTokenExpire();
-        }
-    }
+//    /** @throws \Throwable */
+//    private function isEmpty(string $value): void
+//    {
+//        if (empty($value)) {
+//            throw TokenException::getIsEmpty();
+//        }
+//    }
+//
+//    /** @throws TokenException */
+//    private function isExpireValid(\DateTimeImmutable $expire): void
+//    {
+//        $time = $expire->getTimestamp();
+//
+//        if ($time <= time()) {
+//            throw TokenException::getInvalidTokenExpire();
+//        }
+//    }
 //
 //    protected function generateHash(
 //        string $signingKey,
@@ -69,6 +67,7 @@ class TokenGenerator
 //    }
 
     /**
+     * @TODO get me outta here
      * Original credit to Laravel's Str::random() method.
      */
     public function getRandomAlphaNumStr(int $length): string
@@ -96,13 +95,4 @@ class TokenGenerator
 //            throw TokenException::getBadBytes();
 //        }
 //    }
-
-    protected function encodeHashData(\DateTimeImmutable $expiresAt, string $verifier, string $userId): string
-    {
-        return \json_encode([
-            $verifier,
-            $userId,
-            $expiresAt->format('Y-m-d\TH:i:s')
-        ]);
-    }
 }
