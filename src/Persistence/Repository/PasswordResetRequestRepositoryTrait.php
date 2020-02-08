d37e82b24f4ceb562eda697e08b4f4ca475feb7e
Jesse Rushlow <jr@geeshoe.com>
2020-02-08 09:42:27 -0500

    removed copyright notice
    to be added later

--- src/Persistence/Repository/PasswordResetRequestRepositoryTrait.php
+++ src/Persistence/Repository/PasswordResetRequestRepositoryTrait.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of the Symfony package.
- *
- * (c) Fabien Potencier <fabien@symfony.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace SymfonyCasts\Bundle\ResetPassword\Persistence\Repository;
 
 use Symfony\Component\Security\Core\User\UserInterface;
