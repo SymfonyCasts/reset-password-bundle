643001ae59d8f176130c63ae4c5473888d660f6a
Jesse Rushlow <jr@geeshoe.com>
2020-02-09 09:37:32 -0500

    refactored repository unit test
    remove tests that defined structure early on in dev
    implement alternative to test if trait is compatible with interface

--- src/tests/UnitTests/Persistence/ResetPasswordRequestRepositoryTraitTest.php
+++ src/tests/UnitTests/Persistence/ResetPasswordRequestRepositoryTraitTest.php
@@ -1,45 +1,33 @@
 <?php
 
-declare(strict_types=1);
-
 namespace SymfonyCasts\Bundle\ResetPassword\tests\UnitTests\Persistence;
 
+use SymfonyCasts\Bundle\ResetPassword\Model\ResetPasswordRequestInterface;
 use SymfonyCasts\Bundle\ResetPassword\Persistence\ResetPasswordRequestRepositoryInterface;
-use SymfonyCasts\Bundle\ResetPassword\tests\Fixtures\ResetPasswordRequestRepositoryTraitTestFixture;
 use PHPUnit\Framework\TestCase;
 use SymfonyCasts\Bundle\ResetPassword\Persistence\Repository\ResetPasswordRequestRepositoryTrait;
 
+/**
+ * @author Jesse Rushlow <jr@rushlow.dev>
+ * @author Ryan Weaver <weaverryan@gmail.com>
+ */
 class ResetPasswordRequestRepositoryTraitTest extends TestCase
 {
-    public function methodDataProvider(): \Generator
+    public function testTraitIsCompatibleWithInterface(): void
     {
-        yield ['persistResetPasswordRequest'];
-        yield ['findResetPasswordRequest'];
-        yield ['getMostRecentNonExpiredRequestDate'];
-        yield ['removeResetPasswordRequest'];
+        $sut = new class implements ResetPasswordRequestRepositoryInterface{
+            use ResetPasswordRequestRepositoryTrait;
+
+            public function createResetPasswordRequest(
+                object $user,
+                \DateTimeInterface $expiresAt,
+                string $selector,
+                string $hashedToken
+            ): ResetPasswordRequestInterface {}
+        };
+
+        self::assertInstanceOf(ResetPasswordRequestRepositoryInterface::class, $sut);
     }
-
-    /**
-     * @test
-     * @dataProvider methodDataProvider
-     */
-    public function hasMethod(string $method): void
-    {
-        self::assertTrue(method_exists(ResetPasswordRequestRepositoryTrait::class, $method));
-    }
-
-    /** @test */
-    public function traitIsCompatibleWithInterface(): void
-    {
-        // Ensure fixture implements ResetPasswordRequestRepositoryInterface::class
-        $interfaces = class_implements(ResetPasswordRequestRepositoryTraitTestFixture::class);
-        self::assertArrayHasKey(ResetPasswordRequestRepositoryInterface::class, $interfaces);
-
-        // Ensure fixture uses ResetPasswordRequestRepositoryTrait::class
-        $traits = class_uses(ResetPasswordRequestRepositoryTraitTestFixture::class);
-        self::assertArrayHasKey(ResetPasswordRequestRepositoryTrait::class, $traits);
-
-        // PHP throws fatal error if trait is not compatible with interface
-        new ResetPasswordRequestRepositoryTraitTestFixture();
-    }
+    
+    //@TODO Add unit tests for trait methods in separate PR
 }
