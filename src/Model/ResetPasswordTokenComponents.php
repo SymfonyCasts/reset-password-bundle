4b8bc0f4613cbbee415596cf81973f6a91918f0c
Jesse Rushlow <jr@geeshoe.com>
2020-02-11 11:37:29 -0500

    store token components in internal object

---
+++ src/Model/ResetPasswordTokenComponents.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace SymfonyCasts\Bundle\ResetPassword\Model;
+
+/**
+ * @author Jesse Rushlow <jr@rushlow.dev>
+ * @author Ryan Weaver <weaverryan@gmail.com>
+ * @internal
+ * @final
+ */
+class ResetPasswordTokenComponents
+{
+    /**
+     * @var string non-hashed random string used to fetch request from persistence
+     */
+    private $selector;
+
+    /**
+     * @var string non-hashed string used to verify token
+     */
+    private $verifier;
+
+    /**
+     * @var string
+     */
+    private $hashedToken;
+
+    public function __construct(string $selector, string $verifier, string $hashedToken)
+    {
+        $this->selector = $selector;
+        $this->verifier = $verifier;
+        $this->hashedToken = $hashedToken;
+    }
+
+    public function getSelector(): string
+    {
+        return $this->selector;
+    }
+
+    public function getHashedToken(): string
+    {
+        return $this->hashedToken;
+    }
+
+    public function getPublicToken(): string
+    {
+        return $this->selector . $this->verifier;
+    }
+}
