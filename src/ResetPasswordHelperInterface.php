7214fa3e8ae02658ce52b735bdc683a1c7924ffa
Jesse Rushlow <jr@geeshoe.com>
2020-02-23 17:30:59 -0500

    add summaries & descriptions to the interface
    updated Ryan's email

--- src/ResetPasswordHelperInterface.php
+++ src/ResetPasswordHelperInterface.php
@@ -6,13 +6,31 @@ use SymfonyCasts\Bundle\ResetPassword\Model\ResetPasswordToken;
 
 /**
  * @author Jesse Rushlow <jr@rushlow.dev>
- * @author Ryan Weaver <weaverryan@gmail.com>
+ * @author Ryan Weaver <ryan@symfonycasts.com>
  */
 interface ResetPasswordHelperInterface
 {
+    /**
+     * Generates & persists a new password reset token that can be provided to the user
+     */
     public function generateResetToken(object $user): ResetPasswordToken;
 
+    /**
+     * Validate a reset request and fetch the user from persistence
+     *
+     * The token provided to the user from generateResetToken() is validated
+     * against a ResetPasswordRequest object stored in persistence.
+     *
+     * @param string $fullToken selector + verifier token
+     */
     public function validateTokenAndFetchUser(string $fullToken): object;
 
+    /**
+     * Remove a single ResetPasswordRequest object from persistence
+     *
+     * Intended to be used after validation. Not appropriate for clearing
+     * expired ResetPasswordRequest objects. Use the built in garbage collector
+     * instead.
+     */
     public function removeResetRequest(string $fullToken): void;
 }
