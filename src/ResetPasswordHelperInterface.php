080024dbb6185a8b5db60d615499a56079e44a78
Jesse Rushlow <jr@geeshoe.com>
2020-02-24 15:51:00 -0500

    refactor docblocks

--- src/ResetPasswordHelperInterface.php
+++ src/ResetPasswordHelperInterface.php
@@ -11,31 +11,30 @@ use SymfonyCasts\Bundle\ResetPassword\Model\ResetPasswordToken;
 interface ResetPasswordHelperInterface
 {
     /**
-     * Generate & persist a new password reset token that can be provided to the user
+     * Generate a new ResetPasswordToken that can be provided to the user.
      *
-     * ResetPasswordHelper persists a ResetPasswordRequest object which contains
-     * the hashed token. The ResetPasswordToken object returned by this method
-     * contains the user token which is used to select and verify a persisted
-     * request object.
+     * This method must also persist the token information to storage so that
+     * the validateTokenAndFetchUser() method can verify the token validity
+     * and removeResetRequest() can eventually invalidate it by removing it
+     * from storage.
      */
     public function generateResetToken(object $user): ResetPasswordToken;
 
     /**
-     * Validate a reset request and fetch the user from persistence
+     * Validate a reset request and fetch the user from persistence.
      *
      * The token provided to the user from generateResetToken() is validated
-     * against a ResetPasswordRequest object stored in persistence.
+     * against a token stored in persistence.
      *
      * @param string $fullToken selector string + verifier string provided by the user
      */
     public function validateTokenAndFetchUser(string $fullToken): object;
 
     /**
-     * Remove a single ResetPasswordRequest object from persistence
+     * Remove a password reset request token from persistence
      *
-     * Intended to be used after validation. Not appropriate for clearing
-     * expired ResetPasswordRequest objects. Use the built in garbage collector
-     * instead.
+     * Intended to be used after validation. - this will typically remove
+     * the token from storage so that it can't be used again.
      *
      * @param string $fullToken selector string + verifier string provided by the user
      */
