e616543946feccbc30baa506d8f78f8bd8c40107
Jesse Rushlow <jr@geeshoe.com>
2020-03-06 14:43:55 -0500

    adds copyright header to test suites

--- tests/UnitTests/Persistence/FakeResetPasswordInternalRepositoryTest.php
+++ tests/UnitTests/Persistence/FakeResetPasswordInternalRepositoryTest.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * This file is part of the SymfonyCasts ResetPasswordBundle package.
+ * Copyright (c) SymfonyCasts <https://symfonycasts.com/>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace SymfonyCasts\Bundle\ResetPassword\Tests\UnitTests\Persistence;
 
 use SymfonyCasts\Bundle\ResetPassword\Exception\FakeRepositoryException;
