c869e6d6e92c93c5b3b5e63172c9ddfd896fb40d
bocharsky-bw <bocharsky.bw@gmail.com>
2022-01-28 12:08:22 +0200

    Add error messages hardcoded in Maker as constants

--- src/Exception/ResetPasswordExceptionInterface.php
+++ src/Exception/ResetPasswordExceptionInterface.php
@@ -14,5 +14,8 @@ namespace SymfonyCasts\Bundle\ResetPassword\Exception;
  */
 interface ResetPasswordExceptionInterface extends \Throwable
 {
+    public const MESSAGE_PROBLEM_VALIDATE = 'There was a problem validating your password reset request';
+    public const MESSAGE_PROBLEM_HANDLE = 'There was a problem handling your password reset request';
+
     public function getReason(): string;
 }
