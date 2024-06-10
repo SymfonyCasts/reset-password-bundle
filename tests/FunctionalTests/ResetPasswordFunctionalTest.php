9ff3f871a019b46d3391863e7e60756863b7ccae
Jesse Rushlow <jr@rushlow.dev>
2024-06-10 08:19:36 -0400

    [tests] use internal test helpers

--- tests/FunctionalTests/ResetPasswordFunctionalTest.php
+++ tests/FunctionalTests/ResetPasswordFunctionalTest.php
@@ -11,8 +11,7 @@ namespace SymfonyCasts\Bundle\ResetPassword\Tests\Functional;
 
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Filesystem\Filesystem;
-use Symfony\Component\Process\Exception\ProcessFailedException;
-use Symfony\Component\Process\Process;
+use SymfonyCasts\InternalTestHelpers\TestProcessHelper;
 
 class ResetPasswordFunctionalTest extends TestCase
 {
@@ -33,13 +32,13 @@ class ResetPasswordFunctionalTest extends TestCase
         $fs->mkdir($cachePath);
 
         // Copy bundle to a "repo" dir for tests
-        $this->runProcess(sprintf('git clone %s %s/bundle', $rootPath, $cachePath), $cachePath);
+        TestProcessHelper::runNow(sprintf('git clone %s %s/bundle', $rootPath, $cachePath), $cachePath);
 
         // Install Symfony Skeleton
-        $this->runProcess(sprintf('composer create-project symfony/skeleton %s --prefer-dist', $this->appPath), $cachePath);
+        TestProcessHelper::runNow(sprintf('composer create-project symfony/skeleton %s --prefer-dist', $this->appPath), $cachePath);
 
         // Setup the app as a "webapp" (similar to symfony new --webapp)
-        $this->runProcess('composer require symfony/webapp-pack --prefer-dist', $this->appPath);
+        TestProcessHelper::runNow('composer require symfony/webapp-pack --prefer-dist', $this->appPath);
 
         // Tell composer to use "our" reset-password-bundle instead of fetching it from packagist.
         $composerJson = json_decode(file_get_contents(sprintf('%s/composer.json', $this->appPath)), associative: true, flags: \JSON_THROW_ON_ERROR);
@@ -56,7 +55,7 @@ class ResetPasswordFunctionalTest extends TestCase
 
         file_put_contents(sprintf('%s/composer.json', $this->appPath), json_encode($composerJson, \JSON_THROW_ON_ERROR | \JSON_PRETTY_PRINT | \JSON_UNESCAPED_SLASHES));
 
-        $this->runProcess('composer require symfonycasts/reset-password-bundle', $this->appPath);
+        TestProcessHelper::runNow('composer require symfonycasts/reset-password-bundle', $this->appPath);
 
         $bundleVendorPath = sprintf('%s/vendor/symfonycasts/reset-password-bundle', $this->appPath);
 
@@ -70,7 +69,7 @@ class ResetPasswordFunctionalTest extends TestCase
         $fs->remove(sprintf('%s/var/cache', $this->appPath));
 
         // Use MakerBundle's `make:user` to create a user because I'm too lazy to make a fixture...
-        $this->runProcess('bin/console make:user --is-entity --with-password -n --identity-property-name email User ', $this->appPath);
+        TestProcessHelper::runNow('bin/console make:user --is-entity --with-password -n --identity-property-name email User ', $this->appPath);
 
         // Copy over app fixtures that were "generated" by maker-bundle - we should replace this with make:reset-password like
         // we do above for make:user... reason for the fixtures: lazy
@@ -78,29 +77,12 @@ class ResetPasswordFunctionalTest extends TestCase
         $fs->mirror($fixturesPath, $this->appPath, options: ['override' => true]);
 
         // Setup persistence
-        $this->runProcess('bin/console d:s:create', $this->appPath);
-    }
-
-    private function runProcess(string $cmd, string $workingDir): void
-    {
-        $process = Process::fromShellCommandline($cmd, $workingDir);
-
-        if (0 !== ($exitCode = $process->run())) {
-            dump($process->getErrorOutput());
-
-            throw new \RuntimeException(sprintf('Process Failed - Exit Code: %d - %s Cmd: %s', $exitCode, Process::$exitCodes[$exitCode] ?? '', $cmd));
-        }
+        TestProcessHelper::runNow('bin/console d:s:create', $this->appPath);
     }
 
     public function testApp(): void
     {
-        try {
-            // Run test app tests
-            Process::fromShellCommandline('bin/phpunit', $this->appPath)
-                ->mustRun();
-        } catch (ProcessFailedException $exception) {
-            $this->fail($exception->getMessage());
-        }
+        TestProcessHelper::runNow('bin/phpunit', $this->appPath);
 
         $this->expectNotToPerformAssertions();
     }
