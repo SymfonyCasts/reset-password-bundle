3c5b6859d6711df99ec3f6d28fa8b6029abfe351
Jesse Rushlow <jr@rushlow.dev>
2024-06-13 10:27:04 -0400

    cleanup

--- tests/FunctionalTests/ResetPasswordFunctionalTest.php
+++ tests/FunctionalTests/ResetPasswordFunctionalTest.php
@@ -10,51 +10,42 @@
 namespace SymfonyCasts\Bundle\ResetPassword\Tests\Functional;
 
 use PHPUnit\Framework\TestCase;
-use Symfony\Component\Filesystem\Filesystem;
 use SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle;
 use SymfonyCasts\InternalTestHelpers\AppTestHelper;
 use SymfonyCasts\InternalTestHelpers\TestProcessHelper;
 
-class ResetPasswordFunctionalTest extends TestCase
+final class ResetPasswordFunctionalTest extends TestCase
 {
     private AppTestHelper $appTestHelper;
-    private string $testAppPath;
 
     protected function setUp(): void
     {
         $this->appTestHelper = (new AppTestHelper(SymfonyCastsResetPasswordBundle::class))
             ->init('symfonycasts/reset-password-bundle')
         ;
+    }
 
-//        $bundleVendorPath = sprintf('%s/vendor/symfonycasts/reset-password-bundle', $this->appTestHelper->getTestAppPath());
-//
-//        // DX - So we can test local changes without having to commit them.
-//        if (!is_link($bundleVendorPath)) {
-//            $fs->remove($bundleVendorPath);
-//            $fs->symlink($this->appTestHelper->getCachedBundlePath(), $bundleVendorPath);
-//        }
-//
-//        // Ensure the app has fresh cache on the first test run
-//        $fs->remove(sprintf('%s/var/cache', $this->appTestHelper->getTestAppPath()));
-
-        $this->testAppPath = $this->appTestHelper->createAppForTest();
+    public function testAppResetPasswordWorksInAWebApp(): void
+    {
+        $appPath = $this->appTestHelper->createAppForTest();
 
         // Use MakerBundle's `make:user` to create a user because I'm too lazy to make a fixture...
-        TestProcessHelper::runNow('bin/console make:user --is-entity --with-password -n --identity-property-name email User ', $this->testAppPath);
+        TestProcessHelper::runNow('bin/console make:user --is-entity --with-password -n --identity-property-name email User ', $appPath);
 
         // Copy over app fixtures that were "generated" by maker-bundle - we should replace this with make:reset-password like
         // we do above for make:user... reason for the fixtures: lazy
         $fixturesPath = sprintf('%s/tests/Fixtures/App', $this->appTestHelper->rootPath);
-        $this->appTestHelper->fs->mirror($fixturesPath, $this->testAppPath, options: ['override' => true]);
+        $this->appTestHelper->fs->mirror($fixturesPath, $appPath, options: ['override' => true]);
 
         // Setup persistence
-        TestProcessHelper::runNow('bin/console d:s:create', $this->testAppPath);
-    }
+        TestProcessHelper::runNow('bin/console d:s:create', $appPath);
 
-    public function testApp(): void
-    {
-        TestProcessHelper::runNow('bin/phpunit', $this->testAppPath);
+        // Run the unit tests (Fixtures/App/tests) in the web app.
+        TestProcessHelper::runNow('bin/phpunit', $appPath);
 
+        // If any of the tests within the app fail, an exception is thrown and
+        // "this" test will fail. But we need this assertion because we are not
+        // actually performing any assertions directly.
         $this->expectNotToPerformAssertions();
     }
 }
