9fc3a832513e06390c381298033f79c656b9895c
Jesse Rushlow <jr@rushlow.dev>
2024-07-30 04:53:49 -0400

    [ci] php-cs-fixer it up

--- src/ResetPasswordHelper.php
+++ src/ResetPasswordHelper.php
@@ -72,7 +72,7 @@ class ResetPasswordHelper implements ResetPasswordHelperInterface
 
         $resetRequestLifetime = $resetRequestLifetime ?? $this->resetRequestLifetime;
 
-        $expiresAt = new \DateTimeImmutable(sprintf('+%d seconds', $resetRequestLifetime));
+        $expiresAt = new \DateTimeImmutable(\sprintf('+%d seconds', $resetRequestLifetime));
 
         $generatedAt = ($expiresAt->getTimestamp() - $resetRequestLifetime);
 
@@ -164,7 +164,7 @@ class ResetPasswordHelper implements ResetPasswordHelperInterface
     public function generateFakeResetToken(?int $resetRequestLifetime = null): ResetPasswordToken
     {
         $resetRequestLifetime = $resetRequestLifetime ?? $this->resetRequestLifetime;
-        $expiresAt = new \DateTimeImmutable(sprintf('+%d seconds', $resetRequestLifetime));
+        $expiresAt = new \DateTimeImmutable(\sprintf('+%d seconds', $resetRequestLifetime));
 
         $generatedAt = ($expiresAt->getTimestamp() - $resetRequestLifetime);
 
